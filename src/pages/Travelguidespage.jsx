c9962c113599aad9e8cd2c53d4193d00bcf274ad
diegoromarioH <diegoromario.work@gmail.com>
2026-09-06 19:18:20 -0600

    Remove incorrectly cased Travelguidespage file

--- src/pages/Travelguidespage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import DataTable from '../components/DataTable';
-import travelGuides from '../config/tables/travelGuides';
-
-export default function TravelGuidesPage() {
-  return <DataTable table={travelGuides} />;
-}
