7ae5ba4c92440bad7d461771e512e5131a3f594d
diegoromarioH <diegoromario.work@gmail.com>
2026-09-06 19:17:52 -0600

    Remove incorrectly cased Destinationspage file

--- src/pages/Destinationspage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import DataTable from '../components/DataTable';
-import destinations from '../config/tables/destinations';
-
-export default function DestinationsPage() {
-  return <DataTable table={destinations} />;
-}
